6e8bb772a4802fffee8be673a74b1451e53fb430
Mr-Yan99999 <18017870857@163.com>
2019-11-18 23:16:18 +0800

    增加游戏功能

---
+++ Games/alien_invasion.py
@@ -0,0 +1,23 @@
+import pygame
+from Games.settings import Settings
+from Games.ship import Ship
+import Games.game_functions as gf
+
+def run_game():
+
+    #初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(ai_settings,screen)
+
+    #开始游戏主循环
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+
+run_game()
