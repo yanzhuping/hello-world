d3e62b2b8b7607f89b6d62ec14a837327ac19d4d
yanzhuping <yanzhuping@angelalign.com>
2019-11-19 18:16:07 +0800

    完善游戏

--- Games/alien_invasion.py
+++ Games/alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 from Games.settings import Settings
 from Games.ship import Ship
 import Games.game_functions as gf
+from pygame.sprite import Group
+from Games.bullet import *
 
 def run_game():
 
@@ -13,11 +15,20 @@ def run_game():
 
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
 
     #开始游戏主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+
+        #删除已经消失的子弹
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
