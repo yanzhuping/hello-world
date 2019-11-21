3995a60cdfb70614201b216e4a639922f0f88364
yanzhuping <yanzhuping@angelalign.com>
2019-11-21 18:17:46 +0800

    增加新功能

--- Games/alien_invasion.py
+++ Games/alien_invasion.py
@@ -3,8 +3,6 @@ from Games.settings import Settings
 from Games.ship import Ship
 import Games.game_functions as gf
 from pygame.sprite import Group
-from Games.bullet import *
-from Games.alien import Alien
 
 def run_game():
 
@@ -18,14 +16,18 @@ def run_game():
     ship = Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
-    #创建一个外星人
-    alien = Alien(ai_settings,screen)
+    #创建一个外星人编组
+    aliens = Group()
+
+    #创建外形人群
+    gf.create_fleet(ai_settings,screen,aliens)
 
     #开始游戏主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+
 
 run_game()
