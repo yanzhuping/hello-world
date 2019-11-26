d9b743c5e19e89bd0d382b52c6fe5dd16e730e30
yanzhuping <yanzhuping@angelalign.com>
2019-11-26 18:14:39 +0800

    增加游戏功能

--- Games/settings.py
+++ Games/settings.py
@@ -1,22 +1,40 @@
 class Settings():
     '''存储游戏所有设置类'''
     def __init__(self):
-        '''初始化游戏设置'''
+        '''初始化游戏静态设置'''
         #屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255,255,255)
+
         #飞船设置
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
+
         #子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 3
+
         #外星人设置
-        self.alien_speed_factor = 10
         self.fleet_drop_speed = 10
-        #fleet_direction为1表示向右移，为-1表示向左移
+
+        #以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        '''初始化岁游戏进行而变化的设置'''
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        #fleet_direction为1表示向右，为-1表示向左
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        '''提高速度设置'''
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
