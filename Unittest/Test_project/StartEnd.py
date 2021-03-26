4cbe6154f67fd6ab96808563b0819dc43f9dcdc1
yanzhuping <yanzhuping@angelalign.com>
2021-03-26 11:43:17 +0800

    update

--- Unittest/Test_project/StartEnd.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class Setup_tearDown(unittest.TestCase):
-    def setUp(self) -> None:
-        print("satr test")
-
-    def tearDown(self) -> None:
-        print("test end")
--- Unittest/Test_project/runtest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-test_dir="./"
-discovery=unittest.defaultTestLoader.discover(test_dir,pattern="test*.py")
-
-if __name__ == '__main__':
-    runner=unittest.TextTestRunner()
-    runner.run(discovery)
