7fe4501ac23e42f0fb6bb1f1251180f24efb2965
yanzhuping <yanzhuping@angelalign.com>
2021-04-19 11:28:51 +0800

    update performanceTest

--- performanceTest/concurrent_test.py
+++ performanceTest/concurrent_test.py
@@ -2,8 +2,10 @@
 # -*- coding:utf-8 -*-
 
 import requests, time, json, threading, random
-from iorthoAPI.common.createSession import *
-
+# from iorthoAPI.common.createSession import *
+from time import ctime
+import logging
+import re
 
 class Presstest(object):
 
@@ -11,18 +13,38 @@ class Presstest(object):
         self.press_url = press_url
         self.account = account
         self.password = password
-        self.session = getCookie()
+        self.session = requests.session()
+        self.header= {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
 
+    def login(self):
+        logging.captureWarnings(True)
+        base_url = 'https://opm-cas.sh-sit.eainc.com:8443/cas/login?service=https://opm-cas.sh-sit.eainc.com:8443/OPM/shiro-cas'
+        r = self.session.get(base_url, headers=self.header, verify=False)
+
+        strr = r.text
+        pat1 = r'= {execution: "(.*?)", _eventId:'
+        execution = re.findall(pat1, strr)
+        par1 = {'username': self.account, 'password': self.password, 'execution': '%s' % execution[0], '_eventId': 'submit',
+                'oginType': '0'}
+        r1 = self.session.post(base_url, headers=self.header, data=par1, allow_redirects=False, verify=False)
+
+        location = r1.headers['Location']
+        r2 = self.session.get(location, headers=self.header, allow_redirects=False, verify=False)
+
+        r3 = self.session.get(location, headers=self.header, allow_redirects=False, verify=False)
+
+        url = "https://opm-cas.sh-sit.eainc.com:8443/OPM/login/validatelogin"
+        data = {}
+        re4 = self.session.post(url, headers=self.header, data=data, verify=False)
+        
     def testinterface(self):
         '''压测接口'''
-        data = {"crmUserCode": "D202009180002",
-                "userName": "yanry4548",
-                "oldPassWord": "333333",
-                "newPassWord": "333333",
-                "confirmPassWord": "333333"}
+        data = {"accountType": 3,"crmOrgCode": "H201012070002",
+                             "accountName": "接口测试普通助理%s"%ctime()}
         global ERROR_NUM  #出错数
         try:
-            html = self.session.post(self.press_url, data=data)
+            html = self.session.post(self.press_url, data=data,headers=self.header,verify=False)
             print(html.text)
             if html.json().get('msg') != '成功':
                 print(html.json())
@@ -45,6 +67,7 @@ class Presstest(object):
         Threads = []
 
         for i in range(THREAD_NUM): #线程数
+            print("开始线程:",i,ctime())
             t = threading.Thread(target=self.testonework, name="T" + str(i))
             t.setDaemon(True)
             Threads.append(t)
@@ -65,14 +88,15 @@ class Presstest(object):
 
 
 if __name__ == '__main__':
-    press_url = 'https://opm-cas.sh-sit.eainc.com:8443/OPM/doctor/updatePassWord'
+    press_url = 'https://opm-cas.sh-sit.eainc.com:8443/OPM/assistant/addAssistant'
     account = "yanry4548"
     password = "333333"
 
-    THREAD_NUM = 3  # 并发线程总数
+    THREAD_NUM = 10  # 并发线程总数
     ONE_WORKER_NUM = 5  # 每个线程的循环次数
-    LOOP_SLEEP = 4 # 每次请求时间间隔(秒)
+    LOOP_SLEEP = 1 # 每次请求时间间隔(秒)
     ERROR_NUM = 0  # 出错数
 
     obj = Presstest(press_url=press_url, account=account, password=password)
+    obj.login()
     obj.run()
--- performanceTest/test_load.py
+++ performanceTest/test_load.py
@@ -14,20 +14,21 @@ class AdminLoadTest(TaskSet):
         """
         header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
+
         base_url = 'https://opm-cas.sh-sit.eainc.com:8443/cas/login?service=https://opm-cas.sh-sit.eainc.com:8443/OPM/shiro-cas'
         r = self.client.get(base_url, headers=header, verify=False)
+
         strr = r.text
         pat1 = r'= {execution: "(.*?)", _eventId:'
         execution = re.findall(pat1, strr)
         par1 = {'username': 'yanry4548', 'password': '333333', 'execution': '%s' % execution[0], '_eventId': 'submit',
                 'oginType': '0'}
         r1 = self.client.post( base_url, headers=header, data=par1, allow_redirects=False, verify=False)
-        location = r1.headers['Location']
 
+        location = r1.headers['Location']
         r2 = self.client.get(location, headers=header, allow_redirects=False, verify=False)
 
         r3 = self.client.get(location, headers=header, allow_redirects=False, verify=False)
-
 
         url = "https://opm-cas.sh-sit.eainc.com:8443/OPM/login/validatelogin"
         data = {}
@@ -38,7 +39,7 @@ class AdminLoadTest(TaskSet):
         登出实例方法
         :return:
         """
-        self.client.get("https://opm-cas.sh-sit.eainc.com:8443/cas/logout")
+        self.client.get("https://opm-cas.sh-sit.eainc.com:8443/cas/logout?service=https://opm-cas.sh-sit.eainc.com:8443/OPM/shiro-cas")
 
     def on_start(self):
         """
@@ -49,14 +50,14 @@ class AdminLoadTest(TaskSet):
         """
         self.login()
 
-    def on_stop(self):
-        """
-        当任何一个task调度执行之后,
-        on_stop实例方法会被调用
-        后登出
-        :return:
-        """
-        self.logout()
+    # def on_stop(self):
+    #     """
+    #     当任何一个task调度执行之后,
+    #     on_stop实例方法会被调用
+    #     后登出
+    #     :return:
+    #     """
+    #     self.logout()
 
     @task
     def admin_index(self):
@@ -64,7 +65,7 @@ class AdminLoadTest(TaskSet):
         对后台主页进行压测
         :return:
         """
-        self.client.post("https://opm-cas.sh-sit.eainc.com:8443/OPM/assistant/addAssistant",{"accountType": 3,"crmOrgCode": "H201012070002",
+        self.client.post("/assistant/addAssistant",{"accountType": 3,"crmOrgCode": "H201012070002",
                              "accountName": "接口测试普通助理%s"%ctime()}
                          )
 
@@ -73,7 +74,7 @@ class RunLoadTests(HttpUser):
     """
     创建运行压测类
     """
-    # task_set = AdminLoadTest
     tasks = [AdminLoadTest]
     min_wait = 3000
     max_wait = 6000
+    host = "https://opm-cas.sh-sit.eainc.com:8443/OPM"
